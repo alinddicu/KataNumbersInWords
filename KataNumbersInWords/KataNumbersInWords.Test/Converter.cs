f84b5bb31573a7be9a96764f3df01fee1dd46c66
alinddicu <alinddicu@yahoo.com>
2015-05-03 16:52:19 +0200

    tested tenths

--- KataNumbersInWords/KataNumbersInWords.Test/Converter.cs
+++ KataNumbersInWords/KataNumbersInWords.Test/Converter.cs
@@ -7,31 +7,44 @@
     {
         private static readonly Dictionary<int, string> BaseMappings = new Dictionary<int, string>
         {
-            {0, "zero"},
-            {1, "one"},
-            {2, "two"},
-            {3, "three"},
-            {4, "four"},
-            {5, "five"},
-            {6, "six"},
-            {7, "seven"},
-            {8, "eight"},
-            {9, "nine"},
-            {10, "ten"},
-            {11, "eleven"},
-            {12, "twelve"},
-            {13, "thirteen"},
-            {14, "fourteen"},
-            {15, "fiteen"},
-            {16, "sixteen"},
-            {17, "seventeen"},
-            {18, "eighteen"},
-            {19, "nineteen"}
+            {0, "Zero"},
+            {1, "One"},
+            {2, "Two"},
+            {3, "Three"},
+            {4, "Four"},
+            {5, "Five"},
+            {6, "Six"},
+            {7, "Seven"},
+            {8, "Eight"},
+            {9, "Nine"},
+            {10, "Ten"},
+            {11, "Eleven"},
+            {12, "Twelve"},
+            {13, "Thirteen"},
+            {14, "Fourteen"},
+            {15, "Fiteen"},
+            {16, "Sixteen"},
+            {17, "Seventeen"},
+            {18, "Eighteen"},
+            {19, "Nineteen"},
+            {20, "Twenty"},
+            {30, "Thirty"},
+            {40, "Fourty"},
+            {50, "Fifty"},
+            {60, "Sixty"},
+            {70, "Seventy"},
+            {80, "Eighty"},
+            {90, "Ninety"},
         };
 
         public string Convert(int numeral)
         {
             if (0 <= numeral && numeral <= 19)
+            {
+                return BaseMappings[numeral];
+            }
+
+            if (20 <= numeral && numeral <= 90 && numeral % 10 == 0)
             {
                 return BaseMappings[numeral];
             }
--- KataNumbersInWords/KataNumbersInWords.Test/ConverterTest.cs
+++ KataNumbersInWords/KataNumbersInWords.Test/ConverterTest.cs
@@ -6,31 +6,51 @@
     [TestClass]
     public class ConverterTest
     {
+        private Converter _converter;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _converter = new Converter();
+        }
+
         [TestMethod]
         public void WhenConvertFromZeroTo19ThenReturnFromZeroToNineteen()
         {
-            var converter = new Converter();
 
-            Check.That(converter.Convert(0)).IsEqualTo("zero");
-            Check.That(converter.Convert(1)).IsEqualTo("one");
-            Check.That(converter.Convert(2)).IsEqualTo("two");
-            Check.That(converter.Convert(3)).IsEqualTo("three");
-            Check.That(converter.Convert(4)).IsEqualTo("four");
-            Check.That(converter.Convert(5)).IsEqualTo("five");
-            Check.That(converter.Convert(6)).IsEqualTo("six");
-            Check.That(converter.Convert(7)).IsEqualTo("seven");
-            Check.That(converter.Convert(8)).IsEqualTo("eight");
-            Check.That(converter.Convert(9)).IsEqualTo("nine");
-            Check.That(converter.Convert(10)).IsEqualTo("ten");
-            Check.That(converter.Convert(11)).IsEqualTo("eleven");
-            Check.That(converter.Convert(12)).IsEqualTo("twelve");
-            Check.That(converter.Convert(13)).IsEqualTo("thirteen");
-            Check.That(converter.Convert(14)).IsEqualTo("fourteen");
-            Check.That(converter.Convert(15)).IsEqualTo("fiteen");
-            Check.That(converter.Convert(16)).IsEqualTo("sixteen");
-            Check.That(converter.Convert(17)).IsEqualTo("seventeen");
-            Check.That(converter.Convert(18)).IsEqualTo("eighteen");
-            Check.That(converter.Convert(19)).IsEqualTo("nineteen");
+            Check.That(_converter.Convert(0)).IsEqualTo("Zero");
+            Check.That(_converter.Convert(1)).IsEqualTo("One");
+            Check.That(_converter.Convert(2)).IsEqualTo("Two");
+            Check.That(_converter.Convert(3)).IsEqualTo("Three");
+            Check.That(_converter.Convert(4)).IsEqualTo("Four");
+            Check.That(_converter.Convert(5)).IsEqualTo("Five");
+            Check.That(_converter.Convert(6)).IsEqualTo("Six");
+            Check.That(_converter.Convert(7)).IsEqualTo("Seven");
+            Check.That(_converter.Convert(8)).IsEqualTo("Eight");
+            Check.That(_converter.Convert(9)).IsEqualTo("Nine");
+            Check.That(_converter.Convert(10)).IsEqualTo("Ten");
+            Check.That(_converter.Convert(11)).IsEqualTo("Eleven");
+            Check.That(_converter.Convert(12)).IsEqualTo("Twelve");
+            Check.That(_converter.Convert(13)).IsEqualTo("Thirteen");
+            Check.That(_converter.Convert(14)).IsEqualTo("Fourteen");
+            Check.That(_converter.Convert(15)).IsEqualTo("Fiteen");
+            Check.That(_converter.Convert(16)).IsEqualTo("Sixteen");
+            Check.That(_converter.Convert(17)).IsEqualTo("Seventeen");
+            Check.That(_converter.Convert(18)).IsEqualTo("Eighteen");
+            Check.That(_converter.Convert(19)).IsEqualTo("Nineteen");
+        }
+
+        [TestMethod]
+        public void WhenConvertFrom20To90ThenReturnFromTwentyToNinety()
+        {
+            Check.That(_converter.Convert(20)).IsEqualTo("Twenty");
+            Check.That(_converter.Convert(30)).IsEqualTo("Thirty");
+            Check.That(_converter.Convert(40)).IsEqualTo("Fourty");
+            Check.That(_converter.Convert(50)).IsEqualTo("Fifty");
+            Check.That(_converter.Convert(60)).IsEqualTo("Sixty");
+            Check.That(_converter.Convert(70)).IsEqualTo("Seventy");
+            Check.That(_converter.Convert(80)).IsEqualTo("Eighty");
+            Check.That(_converter.Convert(90)).IsEqualTo("Ninety");
         }
     }
 }
