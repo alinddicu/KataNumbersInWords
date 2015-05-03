a20d462b6dbdf13bbc248a3edd3e4b9183faf3a3
alinddicu <alinddicu@yahoo.com>
2015-05-03 16:40:32 +0200

    0 to 19 conversion

---
+++ KataNumbersInWords/KataNumbersInWords.Test/Converter.cs
@@ -0,0 +1,42 @@
+﻿namespace KataNumbersInWords
+{
+    using System.Collections.Generic;
+    using System.Globalization;
+
+    public class Converter
+    {
+        private static readonly Dictionary<int, string> BaseMappings = new Dictionary<int, string>
+        {
+            {0, "zero"},
+            {1, "one"},
+            {2, "two"},
+            {3, "three"},
+            {4, "four"},
+            {5, "five"},
+            {6, "six"},
+            {7, "seven"},
+            {8, "eight"},
+            {9, "nine"},
+            {10, "ten"},
+            {11, "eleven"},
+            {12, "twelve"},
+            {13, "thirteen"},
+            {14, "fourteen"},
+            {15, "fiteen"},
+            {16, "sixteen"},
+            {17, "seventeen"},
+            {18, "eighteen"},
+            {19, "nineteen"}
+        };
+
+        public string Convert(int numeral)
+        {
+            if (0 <= numeral && numeral <= 19)
+            {
+                return BaseMappings[numeral];
+            }
+
+            return numeral.ToString(CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ KataNumbersInWords/KataNumbersInWords.Test/ConverterTest.cs
@@ -0,0 +1,36 @@
+﻿namespace KataNumbersInWords
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using NFluent;
+
+    [TestClass]
+    public class ConverterTest
+    {
+        [TestMethod]
+        public void WhenConvertFromZeroTo19ThenReturnFromZeroToNineteen()
+        {
+            var converter = new Converter();
+
+            Check.That(converter.Convert(0)).IsEqualTo("zero");
+            Check.That(converter.Convert(1)).IsEqualTo("one");
+            Check.That(converter.Convert(2)).IsEqualTo("two");
+            Check.That(converter.Convert(3)).IsEqualTo("three");
+            Check.That(converter.Convert(4)).IsEqualTo("four");
+            Check.That(converter.Convert(5)).IsEqualTo("five");
+            Check.That(converter.Convert(6)).IsEqualTo("six");
+            Check.That(converter.Convert(7)).IsEqualTo("seven");
+            Check.That(converter.Convert(8)).IsEqualTo("eight");
+            Check.That(converter.Convert(9)).IsEqualTo("nine");
+            Check.That(converter.Convert(10)).IsEqualTo("ten");
+            Check.That(converter.Convert(11)).IsEqualTo("eleven");
+            Check.That(converter.Convert(12)).IsEqualTo("twelve");
+            Check.That(converter.Convert(13)).IsEqualTo("thirteen");
+            Check.That(converter.Convert(14)).IsEqualTo("fourteen");
+            Check.That(converter.Convert(15)).IsEqualTo("fiteen");
+            Check.That(converter.Convert(16)).IsEqualTo("sixteen");
+            Check.That(converter.Convert(17)).IsEqualTo("seventeen");
+            Check.That(converter.Convert(18)).IsEqualTo("eighteen");
+            Check.That(converter.Convert(19)).IsEqualTo("nineteen");
+        }
+    }
+}
